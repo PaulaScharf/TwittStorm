e2e2b9c312491c486f57481cc6c76d72d96f1922
DrSherwood <benny.riekehotmail.de>
2019-11-07 15:45:25 +0100

    The streaming starts in the project dir by typing node twitter.js

---
+++ twitter.js
@@ -0,0 +1,18 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'MrvR3iGRtOYWEHvyX1GQvbgbn',
+  consumer_secret: '8fDd8OEVcklCC4a0JdWa5P7dOC3J3p029jhFSITbwmDwnVN8CA',
+  access_token_key: '1186999137541283840-8t6BUIDVX9it9aZznjRX7QmIduO4dw',
+  access_token_secret: 'W9b9MvmH93UTbiTcEMr1h2IBqLc2lQfF3LtqphfdeReJK'
+});
+
+client.stream('statuses/filter', {track: 'jesus'}, function(stream) {
+  stream.on('data', function(event) {
+    console.log(event && event.text);
+  });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
