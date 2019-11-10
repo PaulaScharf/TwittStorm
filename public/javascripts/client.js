1042f089fdb0db96d31ea93efe0bef3b2f59e9b2
KPoppi <52214998+KPoppi@users.noreply.github.com>
2019-11-10 15:35:40 +0100

    Delete client.js

--- public/javascripts/client.js
+++
@@ -1,9 +0,0 @@
-// jshint esversion: 6
-// jshint maxerr: 1000
-
-"use strict";  // JavaScript code is executed in "strict mode"
-
-/**
-* @desc TwittStorm, Geosoftware 2, WiSe 2019/2020
-* @author Jonathan Bahlmann, Katharina Poppinga, Benjamin Rieke, Paula Scharf
-*/
