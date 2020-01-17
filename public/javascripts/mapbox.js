4b74f0fd98d665cd5024d7d9fe044b6111285395
jonathom <45635480+jonathom@users.noreply.github.com>
2020-01-17 12:27:05 +0100

    deleted deleteFromURL - Test

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -409,10 +409,6 @@ function getAndUseAOIFromURL(draw) {
 */
 function requestAndDisplayAllRainRadar(map, product) {
 
-	deleteFromURL("mapZoom");
-	deleteFromURL("radProd");
-	deleteFromURL("aoi");
-
 	// is there a timestamp?
 	let currentTimestamp = Date.now();
 	if(typeof paramArray.timestamp === "undefined") {
